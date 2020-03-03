f6a7a1c24745a220de7dc22a499db952024c7c2d
Hasmasan Dragos <hasmasandragos13@gmail.com>
2020-03-03 23:14:55 +0200

    Resolved the client-side archive upload.

--- react_frontend/src/components/DatasetsView.js
+++ react_frontend/src/components/DatasetsView.js
@@ -21,29 +21,64 @@ class DatasetForm extends Component {
         super(props);
         this.state = {
           datasetName: null,
-          datasetDescription: null
+          datasetDescription: null,
+          datasetArchive: null
         };
     }
 
+    handleTextChange(event, stateName){
+        let newState = {};
+        newState[stateName] = event.target.value;
+        this.setState(newState);
+    }
+
+    handleFileChange(event, stateName){
+        let newState = {};
+        newState[stateName] = event.target.files[0];
+        this.setState(newState);
+        event.preventDefault();
+    }
+
     handleSubmit = (event) => {
-        alert("Form submission!");
+        const { datasetName, datasetDescription, datasetArchive } = this.state;
+        console.log(datasetArchive);
+
+        let formData = new FormData();
+        let metadataDict = {"type": "create_dataset", "data": {"name": datasetName, "description": datasetDescription}};
+        formData.append('metadata', JSON.stringify(metadataDict));
+        formData.append('dataset', datasetArchive);
+
+        console.log(formData);
+
         event.preventDefault();
     };
 
     render() {
 
-        return <form onSubmit={(event) => this.handleSubmit(event)}>
+        return <form
+            onSubmit={(event) => this.handleSubmit(event)}
+            style={styles.datasetPreview}
+            encType="multipart/form-data"
+        >
+
             <h1>Upload a new dataset:</h1>
             <label>Name:</label>
             <input
                 type="text"
                 value={this.state.datasetName}
+                onChange={(event) => this.handleTextChange(event, 'datasetName')}
             />
 
             <label>Description:</label>
             <input
                 type="text"
                 value={this.state.datasetDescription}
+                onChange={(event) => this.handleTextChange(event, 'datasetDescription')}
+            />
+            <input
+                type="file"
+                onChange={(event) => this.handleFileChange(event, 'datasetArchive')}
+                name='datasetArchive'
             />
 
             <input type="submit" value="Submit"/>
