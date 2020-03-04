33f3969e842d72c4ad2300419e532092002d9c10
Hasmasan Dragos <hasmasandragos13@gmail.com>
2020-03-04 11:22:32 +0200

    Prototype for client-side denoiser creation.

--- react_frontend/src/components/DenoisersView.js
+++ react_frontend/src/components/DenoisersView.js
@@ -17,6 +17,78 @@ class DenoiserView extends Component{
     }
 }
 
+class LayersList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            layers: []
+        }
+    }
+
+    render() {
+
+
+        return <div>
+            <ul>
+
+            </ul>
+            <button>
+                Add new layer
+            </button>
+        </div>
+    }
+}
+
+class LayerProperties extends Component {
+    render() {
+        return <p>Layer props</p>
+    }
+}
+
+class LayerForm extends Component {
+    render() {
+        return <p>Layer</p>
+    }
+}
+
+
+class DenoisersForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          denoiserName: null,
+          denoiserDescription: null,
+          denoiserTrainable: true,
+          denoiserStructure: {
+              type: null,
+              input_shape: null,
+              optimizer: null,
+              loss_function: null,
+          }
+        };
+    }
+
+    handleSubmit = (event) => {
+        alert("Form submission!");
+        event.preventDefault();
+    };
+
+    render() {
+
+        return <form onSubmit={(event) => this.handleSubmit(event)}>
+            <h1>Upload a new denoiser:</h1>
+            <label>Name:</label>
+            <input
+                type="text"
+                value={this.state.denoiserName}
+            />
+            <LayersList/>
+            <input type="submit" value="Submit"/>
+        </form>
+    }
+}
+
+
 class DenoisersView extends Component{
     constructor(props){
         super(props);
