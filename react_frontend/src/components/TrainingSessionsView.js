7da1e6b07118e5c7cf64e2688c24e004c217a7db
Hasmasan Dragos <hasmasandragos13@gmail.com>
2020-03-29 20:24:23 +0300

    Implemented early training session creation on frontend

--- react_frontend/src/components/TrainingSessionsView.js
+++ react_frontend/src/components/TrainingSessionsView.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {theme, httpAddress, listElementStyle, listContainerStyle} from "./Constants";
+import {handleFormFileChange, handleFormFileContentChange, handleFormTextChange} from "./Utils";
 
 class TrainingSessionView extends Component{
     constructor(props){
@@ -21,24 +22,94 @@ class TrainingSessionForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-          trainingSessionName: null,
-          trainingSessionDescription: null
+            trainingSessionName: null,
+            trainingSessionDescription: null,
+            epochs: null,
+            cleanDatasetId: null,
+            noisyDatasetId: null,
+            denoiserId: null,
+            learningStrategyId: null,
         };
+
+        this.handleFormTextChange = handleFormTextChange.bind(this);
+        this.handleFormFileChange = handleFormFileChange.bind(this);
+        this.handleFormFileContentChange = handleFormFileContentChange.bind(this);
     }
 
     handleSubmit = (event) => {
-        alert("Form submission!");
+        const { trainingSessionName, trainingSessionDescription, epochs, cleanDatasetId, noisyDatasetId, denoiserId, learningStrategyId } = this.state;
+
+        let formData = new FormData();
+        let metadataDict =
+            {
+                "type": "create_denoiser",
+                "data": {
+                    "name": trainingSessionName,
+                    "description": trainingSessionDescription,
+                    "epochs": parseInt(epochs, 10),
+                    "clean_dataset_id": parseInt(cleanDatasetId, 10),
+                    "noisy_dataset_id": parseInt(noisyDatasetId, 10),
+                    "denoiser_id": parseInt(denoiserId, 10),
+                    "learning_strategy_id": learningStrategyId,
+                }
+            };
+
+        formData.append('metadata', JSON.stringify(metadataDict));
+
+        fetch(`${httpAddress}/training_sessions`, {
+            mode: 'no-cors',
+            method: 'POST',
+            body: formData
+        })
+        //TO DO: Handle responses accordingly
+            .then((response) => {
+                console.log(`Received response: ${response}`);
+            })
+            .catch((error) => {
+                console.log("Error while fetching messages from the server. Reason: ", error)
+            });
         event.preventDefault();
     };
 
     render() {
 
         return <form onSubmit={(event) => this.handleSubmit(event)}>
-            <h1>Upload a new trainingSession:</h1>
+            <h1>Upload a new Training session:</h1>
             <label>Name:</label>
             <input
                 type="text"
                 value={this.state.trainingSessionName}
+                onChange={(event) => this.handleFormTextChange(event, 'trainingSessionName')}
+            />
+            <label>Description:</label>
+            <input
+                type="text"
+                value={this.state.trainingSessionDescription}
+                onChange={(event) => this.handleFormTextChange(event, 'trainingSessionDescription')}
+            />
+            <label>Epochs:</label>
+            <input
+                type="text"
+                value={this.state.epochs}
+                onChange={(event) => this.handleFormTextChange(event, 'epochs')}
+            />
+            <label>Clean dataset id:</label>
+            <input
+                type="text"
+                value={this.state.cleanDatasetId}
+                onChange={(event) => this.handleFormTextChange(event, 'cleanDatasetId')}
+            />
+            <label>Noisy dataset id:</label>
+            <input
+                type="text"
+                value={this.state.noisyDatasetId}
+                onChange={(event) => this.handleFormTextChange(event, 'noisyDatasetId')}
+            />
+            <label>Denoiser id:</label>
+            <input
+                type="text"
+                value={this.state.denoiserId}
+                onChange={(event) => this.handleFormTextChange(event, 'denoiserId')}
             />
 
             <input type="submit" value="Submit"/>
