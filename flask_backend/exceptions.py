60b517073b6e682cc0f55c5ee8dc3e949eac8b1c
Hasmasan Dragos <hasmasandragos13@gmail.com>
2020-06-20 14:05:19 +0300

    Create exceptions.py

---
+++ flask_backend/exceptions.py
@@ -0,0 +1,14 @@
+class ExceptionResponse(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        exception_dict = dict(self.payload or ())
+        exception_dict['message'] = self.message
+        return exception_dict
